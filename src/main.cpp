94ef9478398f38887907e10688eaa4ede303bdc5
schmehla <50484769+schmehla@users.noreply.github.com>
2022-01-15 12:53:45 +0100

    added new command line argument for ball radius

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,27 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-    if (argc > 3) {
+    if (argc > 4) {
         std::cout << "Too many arguments." << std::endl;
         return 0;
-    } else if (argc < 3) {
+    } else if (argc < 4) {
         std::cout << "Not enough arguments." << std::endl;
         return 0;
     }
-    std::string inputPath(argv[1]);
-    std::string outputPath(argv[2]);
+    float ballRadius = std::stof(argv[1]);
+    std::string inputPath(argv[2]);
+    std::string outputPath(argv[3]);
     if (!Helpers::pathSyntaxValid(inputPath) || !Helpers::pathSyntaxValid(outputPath)) {
         throw std::runtime_error("Wrong path syntax.");
     }
     Vertices vertices = IO::readVertices(inputPath);
-    Faces faces = BPA::bpa(vertices, 10.0);
+    Faces faces = BPA::bpa(vertices, ballRadius);
+    if (faces.size() <= 0) {
+        std::cout << "no faces" << std::endl;
+    }
     IO::writeMesh(outputPath, vertices, faces);
     return 0;
 }
+
+// TODO check which function inputs could be const
+// TODO put reference AND-sign to type
