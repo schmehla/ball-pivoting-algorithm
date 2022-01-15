0ec367fa798aedf469d3944e8f40868250c3ff0e
schmehla <50484769+schmehla@users.noreply.github.com>
2022-01-15 12:54:16 +0100

    implemented vectors and vector math

--- src/bpa/primitives.cpp
+++ src/bpa/primitives.cpp
@@ -1,5 +1,7 @@
 #include "primitives.h"
 
+#include <cmath>
+
 bool operator==(const Edge edge1, const Edge edge2) {
     return edge1.i == edge2.i && edge1.j == edge2.j;
 }
@@ -14,4 +16,65 @@ bool operator==(const Triangle triangle1, const Triangle triangle2) {
 
 bool operator!=(const Triangle triangle1, const Triangle triangle2) {
     return !(triangle1 == triangle2);
+}
+
+Vector conn(const Vertex from, const Vertex to) {
+    return {
+        to.x - from.x, 
+        to.y - from.y, 
+        to.z - from.z, 
+    };
+}
+
+float dist(const Vertex from, const Vertex to) {
+    return len(conn(from, to));
+}
+
+Vertex toVertex(const Vector vector) {
+    return {
+        vector.x,
+        vector.y,
+        vector.z
+    };
+}
+
+Vector operator+(const Vector vector1, const Vector vector2) {
+    return {
+        vector1.x + vector2.x,
+        vector1.y + vector2.y,
+        vector1.z + vector2.z
+    };
+}
+
+Vector operator-(const Vector vector1, const Vector vector2) {
+    return vector1 + (-1.f * vector2);
+}
+
+float operator*(const Vector vector1, const Vector vector2) {
+    return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z;
+}
+
+Vector operator*(const float scale, const Vector vector) {
+    return {
+        scale * vector.x,
+        scale * vector.y,
+        scale * vector.z
+    };
+}
+
+Vector cross(const Vector vector1, const Vector vector2) {
+    return {
+        vector1.y*vector2.z - vector1.z*vector1.y,
+        vector1.z*vector2.x - vector1.x*vector2.z,
+        vector1.x*vector2.y - vector1.y*vector2.x
+    };
+}
+
+float len(const Vector vector) {
+    return std::sqrt(vector.x*vector.x + vector.y*vector.y + vector.z*vector.z);
+}
+
+Vector setMag(const Vector vector, const float newMag) {
+    float l = len(vector);
+    return (newMag / l) * vector;
 }
--- src/bpa/primitives.h
+++ src/bpa/primitives.h
@@ -23,6 +23,12 @@ struct Triangle {
     VertexIndex k;
 };
 
+struct Vector {
+    float x;
+    float y;
+    float z;
+};
+
 typedef std::vector<Vertex> Vertices;
 typedef std::vector<Edge> Edges;
 typedef std::vector<Triangle> Faces;
@@ -31,3 +37,15 @@ bool operator==(const Edge edge1, const Edge edge2);
 bool operator!=(const Edge edge1, const Edge edge2);
 bool operator==(const Triangle triangle1, const Triangle triangle2);
 bool operator!=(const Triangle triangle1, const Triangle triangle2);
+
+Vector conn(const Vertex from, const Vertex to);
+float dist(const Vertex from, const Vertex to);
+
+Vertex toVertex(const Vector vector);
+Vector operator+(const Vector vector1, const Vector vector2);
+Vector operator-(const Vector vector1, const Vector vector2);
+float operator*(const Vector vector1, const Vector vector2);
+Vector operator*(const float scale, const Vector vector);
+Vector cross(const Vector vector1, const Vector vector2);
+float len(const Vector vector);
+Vector setMag(const Vector vector, const float newMag);
